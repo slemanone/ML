c8a1c2cce31fe6439948f428b1cf46bafec91ed4
Sleman <146531001+slemanone@users.noreply.github.com>
2025-04-03 10:17:19 +0300

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -27,24 +27,9 @@ def load_and_prep_image_from_file(filename, img_shape=224, scale=True):
   else:
     return img
 
-def load_and_prep_image(img, img_shape=224, scale=True):
-  """
-  Reads in an image from filename, turns it into a tensor and reshapes into
-  (224, 224, 3).
-
-  Parameters
-  ----------
-  filename (str): string filename of target image
-  img_shape (int): size to resize target image to, default 224
-  scale (bool): whether to scale pixel values to range(0, 1), default True
-  """
-  # Resize the image
-  img = tf.image.resize(img, [img_shape, img_shape])
-  if scale:
-    # Rescale the image (get all values between 0 and 1)
-    return img/255.
-  else:
-    return img
+def resize_img(image, label, shape = 224,):
+  image = tf.image.resize(image, [shape, shape])
+  return tf.cast(image, tf.float32) , label
 
 
 # Note: The following confusion matrix code is a remix of Scikit-Learn's 
